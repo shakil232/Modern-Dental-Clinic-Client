859b2a8d5395555000cfe70b524baeff4c272c90
shakil232 <dxshakil2@gmail.com>
2022-05-13 19:12:12 -0700

    setup Complete

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
 
 import './App.css';
 
+// import {
+//   BrowserRouter as Router,
+//   Switch,
+//   Route,
+//   Link
+// } from "react-router-dom";
 
 function App() {
-  
+  // <Router>
+  //   <Routes>
+  //     <Route path="/about" element={<About />} />
+  //   </Routes>
+  // </Router>
+
+  // import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+  // import { faCoffee } from '@fortawesome/free-solid-svg-icons'
+
+  // <FontAwesomeIcon icon={faCoffee} />
 
   return (
     <div >
-     
+
     </div>
   );
 }
